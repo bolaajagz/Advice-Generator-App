24eebaf1b3f0a5819068ec7de1f99b2da1fc78b2
bolaajagz <bolaajagunna@gmail.com>
2022-05-30 15:21:36 +0100

    initial update

---
+++ app.js
@@ -0,0 +1,53 @@
+
+// const button = document.querySelector(".main_card_image2")
+
+// button.addEventListener('click', generate)
+
+// function generate() {
+    
+    
+//     const file = "https://api.adviceslip.com/advice"
+    
+//     fetch(file)
+//     .then(x => x.text())
+//     .then(y => 
+//         document.getElementById("main_card_advice").innerHTML = y
+//         );
+// }
+// THE BASIC SYNTAX OF THE FETCH API
+
+const title = document.querySelector("#advicenumber");
+const content = document.querySelector("#textadvice");
+const button = document.querySelector(".main_card_image2");
+
+function generate(){
+
+    const file = "https://api.adviceslip.com/advice"
+
+    fetch(file)
+
+    .then(response =>
+         response.json()
+    )
+
+    .then(information => {
+        const num = information.slip.id
+        const text = information.slip.advice;
+
+        title.innerHTML =  num;
+        content.innerHTML =  text;
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
+
+}
+
+button.addEventListener('click', ()=>{
+    generate()
+})
+
+window.onload = ()=>{
+    generate()
+}
